18fd4b5efff83bf94c7107ddfb01396f2dc0a002
atik <4tiknugraha@gmail.com>
2023-07-19 19:55:17 +0700

    first commit

---
+++ src/api/payment-call-back/controllers/payment-call-back.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * payment-call-back controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::payment-call-back.payment-call-back');
